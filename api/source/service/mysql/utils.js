69cc452178c6552547155eb1c18423b4944e1bee
cd-rite <61710958+cd-rite@users.noreply.github.com>
2021-07-29 11:07:07 -0400

    removed fs, path from package.json, reverted debug changes to mysql utils

--- api/source/service/mysql/utils.js
+++ api/source/service/mysql/utils.js
@@ -16,8 +16,7 @@ module.exports.testConnection = async function () {
     return result[0].version
   }
   catch (err) {
-    console.log(err.message)
-    console.log(err.stack)
+    // console.log(err.message)
     throw (err)
   }
 }
@@ -63,7 +62,6 @@ module.exports.initializeDatabase = async function () {
 
     // Create the connection pool
     const poolConfig = getPoolConfig()
-    console.log(poolConfig)
     _this.pool = mysql.createPool(poolConfig)
     // Set common session variables
     _this.pool.on('connection', function (connection) {
@@ -80,7 +78,6 @@ module.exports.initializeDatabase = async function () {
         process.exit(0);
       } catch(err) {
         console.error(err.message);
-        console.log(err.message);
         process.exit(1);
       }
     }   
@@ -95,7 +92,7 @@ module.exports.initializeDatabase = async function () {
       minTimeout: 5 * 1000,
       maxTimeout: 5 * 1000,
       onRetry: (error) => {
-        console.log(`[DB] ${error.message} ${error.stack}`)
+        console.log(`[DB] ${error.message}`)
       }
     })
     console.log('[DB] Preflight connection succeeded.')
@@ -146,8 +143,6 @@ module.exports.initializeDatabase = async function () {
 
   }
   catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
     throw (err)
   }  
 }
