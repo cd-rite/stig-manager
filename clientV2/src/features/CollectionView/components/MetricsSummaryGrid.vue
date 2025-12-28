47fa8730facb492a6ab6f0dcf10351b131f4cd70
cd-rite <github-rite@rite-solutions.com>
2025-12-28 12:53:05 -0500

    tweaking passthrough styles

--- clientV2/src/features/CollectionView/components/MetricsSummaryGrid.vue
+++ clientV2/src/features/CollectionView/components/MetricsSummaryGrid.vue
@@ -9,7 +9,8 @@
     :pt="{
         table: { style: 'min-width: 50rem; table-layout: fixed' },
       column: {
-          headerCell: { style: 'border-right: 1px solid var(--p-datatable-border-color); border-top: 1px solid var(--p-datatable-border-color)' }   
+          headerCell: { style: 'border-right: 1px solid var(--p-datatable-border-color); border-top: 1px solid var(--p-datatable-border-color)' },
+          bodyCell: { style: 'padding: 4px 12px' }  /* Add this */
         }
       }"
   >
