6e68c2e542928b8cf45a58085df08ec256c931f2
cd-rite <github-rite@rite-solutions.com>
2026-01-03 16:31:48 -0500

    refactor: improve popover hide behavior with delay and cancellation

--- clientV2/src/features/CollectionView/components/CountColumnWithTooltip.vue
+++ clientV2/src/features/CollectionView/components/CountColumnWithTooltip.vue
@@ -10,13 +10,34 @@ defineProps({
 
 const popoverRef = ref()
 const popoverItems = ref([])
+let hideTimeout = null
 
 function showPopover(event, items) {
+  // Cancel any pending hide
+  if (hideTimeout) {
+    clearTimeout(hideTimeout)
+    hideTimeout = null
+  }
   popoverItems.value = items || []
   popoverRef.value.show(event)
 }
 
+function scheduleHide() {
+  // Delay hiding to allow mouse to move to popover
+  hideTimeout = setTimeout(() => {
+    popoverRef.value.hide()
+  }, 150)
+}
+
+function cancelHide() {
+  if (hideTimeout) {
+    clearTimeout(hideTimeout)
+    hideTimeout = null
+  }
+}
+
 function hidePopover() {
+  cancelHide()
   popoverRef.value.hide()
 }
 </script>
@@ -27,12 +48,16 @@ function hidePopover() {
       <span
         class="count-display"
         @mouseenter="showPopover($event, slotProps.data[slotProps.field])"
-        @mouseleave="hidePopover"
+        @mouseleave="scheduleHide"
       >
         {{ Array.isArray(slotProps.data[slotProps.field]) ? slotProps.data[slotProps.field].length : slotProps.data[slotProps.field] }}
       </span>
       <Popover ref="popoverRef">
-        <div class="popover-content">
+        <div
+          class="popover-content"
+          @mouseenter="cancelHide"
+          @mouseleave="hidePopover"
+        >
           <ul class="item-list">
             <li v-for="item in popoverItems" :key="item">
               {{ item }}
